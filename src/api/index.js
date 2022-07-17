ce8ed9393d0bb483d0d49cc31d47579b5bdddab4
kincasasbuenas <kincasasbuenas@hotmail.com>
2022-07-17 13:31:17 -0500

    feat: [RX-011] add redux-toolkit and first slice

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export const getPokemon = () => {
-    return axios.get('https://pokeapi.co/api/v2/pokemon?limit=150')
+    return axios.get('https://pokeapi.co/api/v2/pokemon?limit=20')
         .then( (res) => res.data.results)
         .catch((err) => console.log(err));
 }
